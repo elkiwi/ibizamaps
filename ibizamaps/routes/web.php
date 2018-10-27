b603533d918fbc589b7c0427bda7dfb956260969
Peter Young <elkiwi.ibz@gmail.com>
2018-10-27 04:25:38 -0700

    using Index.js

--- ibizamaps/routes/web.php
+++ ibizamaps/routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 }); */
 
 
- Route::get('/page/{id}', 'MarkerController@show');
+ //Route::get('/page/{id}', 'MarkerController@show');
 
 
 //Route::resource('markers', 'MarkerController');
